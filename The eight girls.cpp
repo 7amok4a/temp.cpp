5424df6409a8ae44a75b6650a9587742239d07da
7amok4a <123641786+7amok4a@users.noreply.github.com>
2023-12-19 08:44:44 +0200

    Create The eight girls.cpp

---
+++ The eight girls.cpp
@@ -0,0 +1,6 @@
+for (int di = -1; di <= 1; ++di) {
+    for (int dj = -1; dj <= 1; ++dj) {
+          int x = i + di , y = j + dj;
+          if(x >= 0 && x < n && y >= 0 && y < m)            
+    }
+    }
